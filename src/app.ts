a05e7e1474fe2fb6301aa7dbb11b49485ffbd1b1
Matteo ANDRE <mandre@esrifrance.fr>
2023-08-23 16:59:48 +0200

    [feat]: v1.1 package

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+export function sum(a: number, b: number): number {
+    return a + b;
+}
+
+export function minus(a: number, b: number): number {
+    return a - b;
+}
+
+export function divide(a: number, b: number): number {
+    return a / b;
+}
+
+export function multiple(a: number, b: number): number {
+    return a * b;
+}
