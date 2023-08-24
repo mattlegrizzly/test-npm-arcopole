c3c97bbce19c73f9dbc5a95e318df524232bd325
Matteo ANDRE <mandre@esrifrance.fr>
2023-08-24 10:15:22 +0200

    [function] : add pow

--- src/app.ts
+++ src/app.ts
@@ -13,3 +13,7 @@ export function divide(a: number, b: number): number {
 export function multiple(a: number, b: number): number {
     return a * b;
 }
+
+export function pow(a: number, b: number): number {
+    return a ** b;
+}
